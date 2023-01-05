29ef529c6f3ed9c8228f3a724dc5dbec1f7ebb7b
SINGsyung <make75682@gmail.com>
2023-01-05 16:33:55 +0800

    [feat] 新增ProgressBar component

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,13 @@
+function ProgressBar() {
+  
+  return (
+    <div className="progress-bar">
+      <p>0%</p>
+      <div className="slider">
+        <div className="progress"></div>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;
